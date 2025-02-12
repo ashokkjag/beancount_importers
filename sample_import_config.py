152526724a8789854faec5be33f4e1bdd1316949
Ashok Kumar <ashokkumar.j@gmail.com>
2025-02-12 17:53:55 +0530

    Bank of Baroda CSV parsing

--- sample_import_config.py
+++ sample_import_config.py
@@ -4,9 +4,11 @@ sys.path.insert(0, path.join(path.dirname(__file__)))
 
 from IN.hdfcbank import hdfcbank_csv
 from IN.icicibank import icicibank_xls
+from IN.bankofbaroda import bob_csv
 
 
 CONFIG = [
     hdfcbank_csv.Importer("INR", "Assets:IN:HDFCBank:Savings"),
-    icicibank_xls.Importer("INR", "Assets:IN:ICICIBank:Savings")
+    icicibank_xls.Importer("INR", "Assets:IN:ICICIBank:Savings"),
+    bob_csv.Importer("INR", "Assets:IN:BankOfBaroda:Savings"),
 ]

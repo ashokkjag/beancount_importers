dedb4f598570a16f893e5e1642b9a50ebfd7dc1d
Ashok <60601612+ashokkjag@users.noreply.github.com>
2025-02-13 17:44:22 +0530

    CAS PDF parser (#8)

--- sample_import_config.py
+++ sample_import_config.py
@@ -6,11 +6,13 @@ from IN.hdfcbank import hdfcbank_csv
 from IN.icicibank import icicibank_xls
 from IN.bankofbaroda import bob_csv
 from IN.zerodha import zerodha_tradebook_csv
+from IN.mf_cas import mf_cas_pdf
 
 
 CONFIG = [
     hdfcbank_csv.Importer("INR", "Assets:IN:HDFCBank:Savings"),
     icicibank_xls.Importer("INR", "Assets:IN:ICICIBank:Savings"),
     bob_csv.Importer("INR", "Assets:IN:BankOfBaroda:Savings"),
-    zerodha_tradebook_csv.Importer("INR", "Assets:IN:Investments:Zerodha", "Assets:IN:Zerodha:Cash"),
+    zerodha_tradebook_csv.Importer("INR", "Assets:Investments:Zerodha", "Assets:Zerodha:Cash"),
+    mf_cas_pdf.Importer("PASSWORD", "INR", "Assets:Investments:MF", "Assets:MF:Cash"),
 ]
